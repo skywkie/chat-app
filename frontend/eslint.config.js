a308b8e04faf8e3ae8c6890039bd81ab01be9932
skywkie <dlunusin@gmail.com>
2025-08-24 21:06:09 +0300

    Init client

---
+++ frontend/eslint.config.js
@@ -0,0 +1,23 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import js from "@eslint/js";
+import { globalIgnores } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default tseslint.config([
+  globalIgnores(["docs"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: globals.browser,
+    },
+  },
+]);

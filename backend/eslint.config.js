72d9690195f93409415d2ff129fe2d46069f5317
skywkie <dlunusin@gmail.com>
2025-11-13 01:24:42 +0300

    create pre-commit hook

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -2,11 +2,13 @@ import js from "@eslint/js";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import globals from "globals";
+import { globalIgnores } from "eslint/config";
 
 export default tseslint.config([
   js.configs.recommended,
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  globalIgnores(["node_modules/*", "./src/generated/*"]),
   {
     files: ["**/*.{js,ts}"],
     rules: {
---
+++ frontend/src/libs/router/guardRoute.tsx
@@ -0,0 +1,15 @@
+// const withAuth = (Component) => {
+//   return (props) => {
+//     if (!isAuthenticated()) {
+//       return <Redirect to="/login" />;
+//     }
+//     return <Component {...props} />;
+//   };
+// };
+
+// // Навигация
+// <Route path="/home" component={withAuth(Home)} />;
+
+// // Компонент
+// const Component = (props) => <div></div>;
+// export default withAuth(Component);
